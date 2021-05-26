196f0b7b56354ca44270dd7a87aadcd1eea725a7
Kevin C <kevin.covelli@mail.utoronto.ca>
2021-05-25 20:29:04 -0400

    display item imgs

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ let bc = new BagOfCrafting(pools, meta);
 
 // 'aaaaaaaa' to [1,1,1,1,1,1,1,1]
 let asciiToNum = (s: string) => s.split('').map(c => c.charCodeAt(0) - 0x61);
-let numToAscii = (n: number[]) => String.fromCharCode(...n.map(i=>i+0x61));
+let numToAscii = (n: number[]) => String.fromCharCode(...n.map(i => i + 0x61));
 let recipes = bc.calculateAllRecipes(asciiToNum('bbbbbbbbccccccccddddddddeeeeeeee'));
 for (let [r, c] of recipes) {
     console.log(items?.get(r)?.name, numToAscii(c))
